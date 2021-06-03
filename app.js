422b8f45c00232fb5c9dd3b0495215466201fd99
herophil <philip.herold.01@gmail.com>
2021-06-03 15:29:07 +0200

    merged app and server js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use("/", express.static(path.join(__dirname, "frontend/dist/karotten-kasino")));
-app.use((req, res, next) => res.sendFile(__dirname, "frontend/dist/karotten-kasino", "index.html"));
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http');
-const app = require('./app');
+const express = require('express');
+const path = require('path');
+
+const app = express();
 const port = process.env.PORT || 3000;
+
+app.use("/", express.static(path.join(__dirname, "frontend/dist/karotten-kasino")));
+app.use((req, res, next) => res.sendFile(__dirname, "frontend/dist/karotten-kasino", "index.html"));
 
 app.set('port', port);
 
