15e045274364886a3585a9723e4121f73c4b75ce
herophil <philip.herold.01@gmail.com>
2021-06-03 15:49:16 +0200

    try and error

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(express.static(path.join(__dirname, "frontend/dist/karotten-kasino")));
 
 app.set('port', port);
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/karotten-kasino/index.html"));
+});
+
+app.get('/test', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/dist/karotten-kasino/index.html"));
+});
+
 const server = http.createServer(app);
 
 server.listen(port);
+
+
