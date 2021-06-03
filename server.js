4430c7aeb415cbb230f7a5968add1ab6b79dcfb9
herophil <philip.herold.01@gmail.com>
2021-06-03 15:45:20 +0200

    try and error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use("/", express.static(path.join(__dirname, "frontend/dist/karotten-kasino")));
+app.use(express.static(path.join(__dirname, "frontend/dist/karotten-kasino")));
 
 app.set('port', port);
 
