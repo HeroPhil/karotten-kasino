3aa57bba382bff87709962f46e49f294ecf0b3d6
herophil <philip.herold.01@gmail.com>
2021-06-03 14:04:20 +0200

    added server js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("test");
+
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}`));
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log("server running");
+})
