51bd28419d4f7482029e666fde96eebd41d4ce2f
herophil <philip.herold.01@gmail.com>
2021-06-07 10:42:38 +0200

    added debug client server connection

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DebugComponent } from './debug/debug.component';
+
+import { SocketIoModule, SocketIoConfig, Socket } from 'ngx-socket-io';
+
+const config: SocketIoConfig = {
+  url: "http://localhost:3000", // ?
+  options: {},
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    DebugComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
