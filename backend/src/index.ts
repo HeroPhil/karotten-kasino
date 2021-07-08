f0f058ebafe973a85be1af37d5075f3742e83a54
herophil <philip.herold.01@gmail.com>
2021-07-08 13:31:40 +0200

    fixed backend lint warning

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import { BackendServer } from "./server";
 
 export const server = new BackendServer();
 
-const debugHandler = new DebugHandler();
+new DebugHandler();
 new PriceIsNiceHandler();
 
 server.start();
